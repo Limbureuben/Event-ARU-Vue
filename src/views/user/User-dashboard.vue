e8f3791402b5d1f8b1124c74ae93100f0b0c49f6
Limbureuben <limbureuben4@gmail.com>
2025-05-01 16:08:42 +0300

    changes

--- src/views/user/User-dashboard.vue
+++ src/views/user/User-dashboard.vue
@@ -1,40 +1,64 @@
 <template>
     <header>
-      <!-- Fixed and responsive navbar -->
-      <MDBNavbar expand="lg" light bg="white" container fixed="top">
+      <!-- Navbar -->
+      <MDBNavbar expand="lg" light bg="white" container>
         <MDBNavbarToggler
-          target="navbarExample01"
-          @click="collapse1 = !collapse1"
-        />
-        <MDBCollapse id="navbarExample01" v-model="collapse1">
+        target="#navbarExample02"
+        @click="collapse2 = !collapse2"
+        ></MDBNavbarToggler>
+        <MDBCollapse id="navbarExample02" v-model="collapse2">
           <MDBNavbarNav class="mb-2 mb-lg-0">
-            <MDBNavbarItem to="#" active> Home </MDBNavbarItem>
-            <MDBNavbarItem to="#"> Features </MDBNavbarItem>
-            <MDBNavbarItem to="#"> Pricing </MDBNavbarItem>
-            <MDBNavbarItem to="#"> About </MDBNavbarItem>
+            <MDBNavbarItem to="#" active>
+              Home
+            </MDBNavbarItem>
+            <MDBNavbarItem to="#">
+              Features
+            </MDBNavbarItem>
+            <MDBNavbarItem to="#">
+              Pricing
+            </MDBNavbarItem>
+            <MDBNavbarItem to="#">
+              About
+            </MDBNavbarItem>
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBNavbar>
-  
-      <!-- Jumbotron -->
-      <div class="p-5 text-center bg-light mt-5">
-        <h1 class="mb-3">Heading</h1>
-        <h4 class="mb-3">Subheading</h4>
-        <a class="btn btn-primary" href="#" role="button">Call to action</a>
+      <!-- Navbar -->
+      <!-- Background image -->
+      <div
+        class="p-5 text-center bg-image"
+        style="
+          background-image: url('https://mdbootstrap.com/img/new/slides/041.webp');
+          height: 400px;
+        "
+      >
+        <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+          <div class="d-flex justify-content-center align-items-center h-100">
+            <div class="text-white">
+              <h1 class="mb-3">Heading</h1>
+              <h4 class="mb-3">Subheading</h4>
+              <MDBBtn tag="a" lg outline="light"
+                href="#!"
+                role="button"
+                >Call to action
+              </MDBBtn>
+            </div>
+          </div>
+        </div>
       </div>
+      <!-- Background image -->
     </header>
   </template>
-  
   <script setup lang="ts">
   import {
     MDBNavbar,
     MDBNavbarToggler,
     MDBNavbarNav,
     MDBNavbarItem,
-    MDBCollapse
+    MDBBtn,
+    MDBCollapse,
   } from 'mdb-vue-ui-kit';
   import { ref } from 'vue';
-  
-  const collapse1 = ref(false);
-  </script>
-  
+
+  const collapse2 = ref(false);
+</script>

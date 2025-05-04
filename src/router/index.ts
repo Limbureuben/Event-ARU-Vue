5cb9843bb007011a2fe315724a031d4b923033e9
Limbureuben <limbureuben4@gmail.com>
2025-05-04 18:53:51 +0300

    changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,37 +13,21 @@ import BookRoom from '@/views/user/BookRoom.vue';
 import InactiveRoom from '@/views/admin/InactiveRoom.vue';
 import AdminProfile from '@/views/admin/AdminProfile.vue';
 
-// const routes = [
-//   { path: '/', component: Login },
-//   { path: '/login', component: Login },
-//   { path: '/register', component: Register },
-//   { path: '/rooms', component: Room },
-//   { path: '/bookroom/:id', component: BookRoom, name: 'BookRoom' },
-//   { path: '/profile', component: UserProfile},
-//   { path: '/admin-profile', component: AdminProfile },
-//   { path: '/admin-dashboard', component: AdminDashboard },
-//   { path: '/add-room', component: AddRoom },
-//   { path: '/BookedRooms', component: InactiveRoom },
-//   { path: '/user-dashboard', component: UserDashboard },
-//   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
-// ]
-
 
 const routes = [
   { path: '/', component: Login },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
-  { path: '/rooms', component: Room },
-  { path: '/bookroom/:id', component: BookRoom, name: 'BookRoom' },
-  { path: '/profile', component: UserProfile },
+  { path: '/rooms', component: Room, meta: { requiresAuth: true, role: 'user'} },
+  { path: '/bookroom/:id', component: BookRoom, name: 'BookRoom', meta: { requiresAuth: true, role: 'user'} },
+  { path: '/profile', component: UserProfile, meta: { requiresAuth: true, role: 'user'} },
   { path: '/admin-profile', component: AdminProfile, meta: { requiresAuth: true, role: 'admin' } },
   { path: '/admin-dashboard', component: AdminDashboard, meta: { requiresAuth: true, role: 'admin' } },
   { path: '/add-room', component: AddRoom, meta: { requiresAuth: true, role: 'admin' } },
-  { path: '/BookedRooms', component: InactiveRoom },
+  { path: '/BookedRooms', component: InactiveRoom, meta: { requiresAuth: true, role:'admin' } },
   { path: '/user-dashboard', component: UserDashboard, meta: { requiresAuth: true, role: 'user' } },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
-
 
 const router = createRouter({
   history: createWebHistory(),
